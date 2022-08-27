fd0ea1b21a8011135897fa8dd70ebe7ce9fd3d96
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-27 19:53:26 +0200

    MAJ TOUT VA BIEN

--- src/components/animations/Swiper.jsx
+++ src/components/animations/Swiper.jsx
@@ -6,12 +6,11 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import "../../styles/components/_swiper.scss";
-
 // import required modules
 import { Pagination, Navigation } from "swiper";
 import { useEffect } from "react";
 import { useState } from "react";
+import { BsBasket } from 'react-icons/bs';
 
 
 export default function SwiperMod() {
@@ -32,7 +31,7 @@ export default function SwiperMod() {
 
     useEffect(() => {
         window.addEventListener("resize", resize);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
@@ -51,76 +50,91 @@ export default function SwiperMod() {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div>
+                    <div className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
-                        <img src="http://via.placeholder.com/640x360" alt="" />
-                        <h3>Info 1</h3>
-                        <span>miaou</span>
+                        <div className="swiper-items-img">
+                            <img src="https://media.ldlc.com/r150/ld/products/00/05/92/81/LD0005928160_1_0005973949.jpg" alt="" />
+                        </div>
+                        <div className="swiper-item-title">Apple MacBook Pro M1 Pro (2021) 16" Gris sidéral 32Go/2To (MK183FN/A-32GB-2TB-QWERTY)</div>
+                        <div className="swiper-item-details">
+                            <div className="swiper-item-price">125€<sup>99</sup></div>
+                            <button className="swiper-item-basket"><BsBasket size="25" /></button>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <div className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
-                        <img src="http://via.placeholder.com/640x360" alt="" />
-                        <h3>Info 2</h3>
-                        <span>miaou</span>
+                        <div className="swiper-items-img">
+                            <img src="https://media.ldlc.com/r150/ld/products/00/05/97/36/LD0005973600.jpg" alt="" />
+                        </div>
+                        <div className="swiper-item-title">Epson WorkForce WF-2965DWF</div>
+                        <div className="swiper-item-details">
+                            <div className="swiper-item-price">100€<sup>00</sup></div>
+                            <button className="swiper-item-basket"><BsBasket size="25" /></button>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                <div className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
-                        <img src="http://via.placeholder.com/640x360" alt="" />
-                        <h3>Info 3</h3>
-                        <span>miaou</span>
+                        <div className="swiper-items-img">
+                            <img src="https://media.ldlc.com/r150/ld/products/00/05/97/37/LD0005973746.jpg" alt="" />
+                        </div>
+                        <div className="swiper-item-title">Dell Inspiron 14 5420-995</div>
+                        <div className="swiper-item-details">
+                            <div className="swiper-item-price">899€<sup>95</sup></div>
+                            <button className="swiper-item-basket"><BsBasket size="25" /></button>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 4</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 5</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 6</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 7</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 8</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div>
+                    <a href="/" className="swiper-items">
                         <span className="badge-swiper badge-swiper-nouveautes">Nouveau</span>
                         <img src="http://via.placeholder.com/640x360" alt="" />
                         <h3>Info 9</h3>
                         <span>miaou</span>
-                    </div>
+                    </a>
                 </SwiperSlide>
             </Swiper>
         </>
