f936e5c20d8ba89434100cc42b5e997ca8eb0dc4
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-05 11:15:23 +0200

    add form

--- src/components/LoginAndRegistration.jsx
+++ src/components/LoginAndRegistration.jsx
@@ -15,6 +15,12 @@ const LoginAndRegistration = () => {
         console.log(jsonDataSignIn);
     }
 
+    const handleFormSignUpSubmit = (e) => {
+        e.preventDefault();
+        const jsonDataSignUp = Object.fromEntries(new FormData(e.target));
+        console.log(jsonDataSignUp);
+    }
+
     return (
         <section className='LoginAndRegistration'>
             <div className='container'>
@@ -25,8 +31,8 @@ const LoginAndRegistration = () => {
                     <div className="formBx">
                         <form onSubmit={handleFormSignInSubmit}>
                             <h2>Se connecter</h2>
-                            <input type="text" placeholder="Login" />
-                            <input type="password" placeholder="Password" />
+                            <input name="login" type="text" placeholder="Login" />
+                            <input name="password" type="password" placeholder="Password" />
                             <input type="submit" value="S'identifier" />
                             <p className="signup">Vous n'avez pas de compte ? <a href="/" onClick={(e) => toggleForm(e)}>Créer mon compte</a></p>
                         </form>
@@ -34,12 +40,12 @@ const LoginAndRegistration = () => {
                 </div>
                 <div className="user signupBx">
                     <div className="formBx">
-                        <form>
+                        <form onSubmit={handleFormSignUpSubmit}>
                             <h2>Créer un nouveau compte</h2>
-                            <input type="text" placeholder="Nom d'utilisateur" />
-                            <input type="text" placeholder="Adresse email" />
-                            <input type="password" placeholder="Création du mot de passe" />
-                            <input type="password" placeholder="Confirmation du mot de passe" />
+                            <input name="login" type="text" placeholder="Nom d'utilisateur" />
+                            <input name="mail" type="text" placeholder="Adresse email" />
+                            <input name="password" type="password" placeholder="Création du mot de passe" />
+                            <input name="Confirm password" type="password" placeholder="Confirmation du mot de passe" />
                             <input type="submit" value="S'enregistrer" />
                             <p className="signup">Vous avez déjà un compte ? <a href="/" onClick={(e) => toggleForm(e)}>S'identifier</a></p>
                         </form>
