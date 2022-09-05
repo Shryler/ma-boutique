20fb83ebc3443c1fa30969636055914f1fce2bc0
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-05 10:47:27 +0200

    new form

---
+++ src/components/LoginAndRegistration.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const LoginAndRegistration = () => {
+
+    function toggleForm(e){
+        e.preventDefault();
+        let container = document.querySelector(".LoginAndRegistration");
+        container = container.firstChild;
+        container.classList.toggle('active');
+    }
+
+    return (
+        <section className='LoginAndRegistration'>
+            <div className='container'>
+                <div className="user signinBx">
+                    <div className="imgBx">
+                        <img src="/img/signIn.jpg" alt="" />
+                    </div>
+                    <div className="formBx">
+                        <form>
+                            <h2>Se connecter</h2>
+                            <input type="text" placeholder="Login" />
+                            <input type="password" placeholder="Password" />
+                            <input type="submit" value="S'identifier" />
+                            <p className="signup">Vous n'avez pas de compte ? <a href="/" onClick={(e) => toggleForm(e)}>Créer mon compte</a></p>
+                        </form>
+                    </div>
+                </div>
+                <div className="user signupBx">
+                    <div className="imgBx">
+                    </div>
+                    <div className="formBx">
+                        <form>
+                            <h2>Créer un nouveau compte</h2>
+                            <input type="text" placeholder="Nom d'utilisateur" />
+                            <input type="text" placeholder="Adresse email" />
+                            <input type="password" placeholder="Création du mot de passe" />
+                            <input type="password" placeholder="Confirmation du mot de passe" />
+                            <input type="submit" value="S'enregistrer" />
+                            <p className="signup">Vous avez déjà un compte ? <a href="/" onClick={(e) => toggleForm(e)}>S'identifier</a></p>
+                        </form>
+                    </div>
+                </div>
+                        <img src="/img/signUp.jpg" alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default LoginAndRegistration;
--- src/pages/EcranPC.js
+++ src/pages/EcranPC.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/esm/Container';
-import CarouselPrincipal from '../components/animations/Carousel';
 import Footer from '../components/Footer';
+import LoginAndRegistration from '../components/LoginAndRegistration';
 import NavbarComponent from '../components/Navbar';
 
 const EcranPC = () => {
     return (
         <>
             <NavbarComponent />
-            <Container fluid>
-                    <CarouselPrincipal />
-            </Container>
             <Container>
                 <main>
-                    <section>
-                        <h1>Ecran PC</h1>
-                    </section>
+                    <LoginAndRegistration />
                 </main>
             </Container>
             <Footer />
