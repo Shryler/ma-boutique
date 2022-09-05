ac7a296d7a40532a7ca5c58d4e6a7fbc7b44c9b0
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-05 16:14:50 +0200

    maj produits

--- src/components/FilterProducts.jsx
+++ src/components/FilterProducts.jsx
@@ -44,12 +44,18 @@ const FilterProducts = () => {
                                 </div>
                                 <div className="avis">50 avis</div>
                             </div>
-                            <div className="price">99€</div>
+                            <div className="swiper-item-details price">
+                                <div className='container-stock'>
+                                    <span>Disponible</span>
+                                </div>
+                                <div className='container-price'>
+                                    <div className="swiper-item-price">100€<sup>00</sup></div>
+                                    <ToolTips button={<BsBasket size="25" />} text={"Ajouter au panier"} classSelect={"swiper-item-basket"}></ToolTips>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="products">
-                    <h1>Produits</h1>
+                    {/* faux contenu */}
                     <div className="products-container">
                         <div className="products-img">
                             <img src="https://media.ldlc.com/r150/ld/products/00/05/59/42/LD0005594295_2.jpg" alt="" />
@@ -63,11 +69,40 @@ const FilterProducts = () => {
                                 <div className="avis">50 avis</div>
                             </div>
                             <div className="swiper-item-details price">
-                                <div className="swiper-item-price">100€<sup>00</sup></div>
-                                <ToolTips button={<BsBasket size="25" />} text={"Ajouter au panier"} classSelect={"swiper-item-basket"}></ToolTips>
+                                <div className='container-stock'>
+                                    <span>Disponible</span>
+                                </div>
+                                <div className='container-price'>
+                                    <div className="swiper-item-price">100€<sup>00</sup></div>
+                                    <ToolTips button={<BsBasket size="25" />} text={"Ajouter au panier"} classSelect={"swiper-item-basket"}></ToolTips>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="products-container">
+                        <div className="products-img">
+                            <img src="https://media.ldlc.com/r150/ld/products/00/05/59/42/LD0005594295_2.jpg" alt="" />
+                        </div>
+                        <div className="products-details">
+                            <div className="info">
+                                <div>
+                                    <div className="title">Acer 24.5" LED - Nitro VG252QPbmiipx</div>
+                                    <div className="min-desc">1920 x 1080 pixels - 2 ms (gris à gris) - Format large 16/9 - Dalle IPS - 144 Hz - G-Sync Compatible - HDMI/DisplayPort - Noir</div>
+                                </div>
+                                <div className="avis">50 avis</div>
+                            </div>
+                            <div className="swiper-item-details price">
+                                <div className='container-stock'>
+                                    <span>Disponible</span>
+                                </div>
+                                <div className='container-price'>
+                                    <div className="swiper-item-price">100€<sup>00</sup></div>
+                                    <ToolTips button={<BsBasket size="25" />} text={"Ajouter au panier"} classSelect={"swiper-item-basket"}></ToolTips>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* faux contenu */}
                 </div>
             </div>
         </section>
