479cdf03e2e405e92c5af87b499cb06cfc039c70
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-21 09:20:02 +0200

    add scrollTo

--- APP-React/src/App.js
+++ APP-React/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Register from "./pages/Register";
 
 function App() {
+
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/connexion" element={<Register />} />
--- APP-React/src/components/Category.jsx
+++ APP-React/src/components/Category.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Category() {
 
@@ -11,16 +11,14 @@ function Category() {
             .then(json => setCategory(json));
     }, [])
 
-    const navigate = useNavigate();
-
     return (<>
         <Container className='my-5 category-container'>
             <h3>Périphériques</h3>
             <div className="category-contain">
                 {category.filter(cat => cat.Id_category <= 4).map(cat => {
                     return (
-                        <div key={cat.Id_category} onClick={() => { navigate(`/category/${cat.category}`); }} className="d-flex gap-5 justify-content-center flex-wrap">
-                            <NavLink to="">
+                        <div key={cat.Id_category} className="d-flex gap-5 justify-content-center flex-wrap">
+                            <NavLink to={`/category/${cat.category}`}>
                                 <img src={cat.img} alt={cat.category} />
                                 <p>{cat.category}</p>
                             </NavLink>
@@ -28,12 +26,13 @@ function Category() {
                     );
                 })}
             </div>
+            <hr />
             <h3>Ordinateurs</h3>
             <div className="category-contain">
                 {category.filter(cat => cat.Id_category >= 5 && cat.Id_category <= 6).map(cat => {
                     return (
-                        <div key={cat.Id_category} onClick={() => { navigate(`/category/${cat.category}`); }} className="d-flex gap-5 justify-content-center flex-wrap">
-                            <NavLink to="">
+                        <div key={cat.Id_category} className="d-flex gap-5 justify-content-center flex-wrap">
+                            <NavLink to={`/category/${cat.category}`}>
                                 <img src={cat.img} alt={cat.category} />
                                 <p>{cat.category}</p>
                             </NavLink>
@@ -41,12 +40,13 @@ function Category() {
                     );
                 })}
             </div>
+            <hr />
             <h3>Tablettes</h3>
             <div className="category-contain">
                 {category.filter(cat => cat.Id_category >= 7 && cat.Id_category <= 8).map(cat => {
                     return (
-                        <div key={cat.Id_category} onClick={() => { navigate(`/category/${cat.category}`); }} className="d-flex gap-5 justify-content-center flex-wrap">
-                            <NavLink to="">
+                        <div key={cat.Id_category} className="d-flex gap-5 justify-content-center flex-wrap">
+                            <NavLink to={`/category/${cat.category}`}>
                                 <img src={cat.img} alt={cat.category} />
                                 <p>{cat.category}</p>
                             </NavLink>
