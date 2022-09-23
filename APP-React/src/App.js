396693e30e1fdaefef5b5a8519098711351669f3
Shryler <105290336+Shryler@users.noreply.github.com>
2022-09-23 13:39:12 +0200

    ScrollToTop

--- APP-React/src/App.js
+++ APP-React/src/App.js
@@ -1,16 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import BaseScreen from "./screens/BaseScreen";
 import Home from "./screens/Home";
 import Register from "./screens/Register";
 import PageNotFound from "./screens/PageNotFound";
 import CategoryDetailScreen from "./screens/CategoryDetailScreen";
-=======
-import ScrollToTop from "./components/ScrollToTop";
-import Home from "./pages/Home";
-import PageNotFound from "./pages/PageNotFound";
-import Register from "./pages/Register";
->>>>>>> 479cdf03e2e405e92c5af87b499cb06cfc039c70
+import ScrollToTop from "./components/animations/ScrollToTop";
 
 function App() {
 
--- APP-React/src/screens/BaseScreen.jsx
+++ APP-React/src/screens/BaseScreen.jsx
@@ -4,11 +4,9 @@ import ReseauxSociaux from "../components/ReseauxSociaux";
 import Footer from "../components/Footer";
 import { Container } from "react-bootstrap";
 import ReturnTop from "../components/animations/ReturnTop";
-import ScrollToTop from "../components/animations/ScrollToTop";
 
 const BaseScreen = () => {
     return (<>
-         <ScrollToTop />  {/* Permet le scroll automatique */}
         <NavbarComponent />
         <ReseauxSociaux />
         <ReturnTop />
